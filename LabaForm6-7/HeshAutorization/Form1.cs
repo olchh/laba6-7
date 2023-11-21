c7830df3ba0447b16ef06d0f17e1dfa83f4a1be8
olchh <98604592+olchh@users.noreply.github.com>
2023-11-21 20:27:21 +0300

    Update Form1.cs

--- LabaForm6-7/HeshAutorization/Form1.cs
+++ LabaForm6-7/HeshAutorization/Form1.cs
@@ -1,16 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using SD = System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 using System.Data;
 using System.Security.Cryptography;
-using System.Text.RegularExpressions;
+
 
 namespace HeshAutorization
 {
